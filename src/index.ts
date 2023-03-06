b46791457bbf019ed79f03e10d1a48526e3727f1
Marc Girbau <marc.girbau94@gmail.com>
2023-03-06 21:43:05 +0100

    Create connectDatabase function

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import "./loadEnvironment.js";
 import chalk from "chalk";
 import createDebug from "debug";
 import startServer from "./server/startServer.js";
+import connectDatabase from "./database/connectDatabase.js";
 
 const debug = createDebug("gamelab:*");
 
 const port = process.env.PORT ?? 4000;
+const mongoDdUrl = process.env.MONGODB_CONNECTION_URL;
 
 try {
+  await connectDatabase(mongoDdUrl!);
+  debug(chalk.green("Connected to database"));
+
   await startServer(Number(port));
   debug(chalk.green(`Server listening on port ${port}`));
 } catch (error) {
-  debug(error.message);
+  debug(chalk.red(error.message));
 }
