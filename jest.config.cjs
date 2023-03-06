7f9be30b155b0596dbaa875bbbd054525b946b43
Marc Girbau <marc.girbau94@gmail.com>
2023-03-06 22:19:53 +0100

    Add notFoundError middleware

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,6 +10,6 @@ module.exports = {
     "!src/server/index.ts",
     "!src/server/startServer.ts",
     "!src/loadEnvironment.ts",
-    "!src/database/connectDataBase.ts",
+    "!src/database/connectDatabase.ts",
   ],
 };
