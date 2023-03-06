1339021f177b7e90287993c63a3cbac80f2aaae6
Marc Girbau <marc.girbau94@gmail.com>
2023-03-06 17:18:00 +0100

    Install jest and ts-jest-webcompat-resolver

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/src/**/*.test.ts"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: ["src/**/*.ts", "!src/index.ts"],
+};
