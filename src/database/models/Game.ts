4cf8c1edcb0349b6c326d029072b64229ce54952
Marc Girbau <marc.girbau94@gmail.com>
2023-03-11 17:42:16 +0100

    Create User model

---
+++ src/database/models/Game.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+
+const gameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  backupImage: {
+    type: String,
+    required: true,
+  },
+  about: { type: String, required: true },
+  platforms: { type: [String], required: true },
+  categories: { type: [String], required: true },
+  gameplayTime: { type: Number, required: true },
+  releaseYear: { type: Number, required: true },
+  developer: { type: String, required: true },
+  ageRatig: { type: String, required: true },
+});
+
+const Game = model("Games", gameSchema, "games");
+
+export default Game;
