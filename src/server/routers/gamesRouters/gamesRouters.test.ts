2382fc63fae17323770727be9e4a14d976d82882
Marc Girbau <marc.girbau94@gmail.com>
2023-03-17 14:00:26 +0100

    Test delete game endpoint

--- src/server/routers/gamesRouters/gamesRouters.test.ts
+++ src/server/routers/gamesRouters/gamesRouters.test.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import "../../../loadEnvironment";
+import { secret } from "../../../loadEnvironment";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import jwt from "jsonwebtoken";
 import request from "supertest";
 import mongoose from "mongoose";
 import connectDatabase from "../../../database/connectDatabase";
@@ -7,6 +10,7 @@ import statusCodes from "../../utils/statusCodes";
 import { app } from "../../app";
 import Game from "../../../database/models/Game";
 import { mockWitcherGame } from "../../mocks/gamesMocks";
+import { mockUserId } from "../../mocks/userMocks";
 
 const { success } = statusCodes;
 
@@ -33,14 +37,43 @@ afterEach(async () => {
   jest.clearAllMocks();
 });
 
-describe("Given a GET /games endpoint", () => {
-  const gamesUrl = "/games";
+const user = mockUserId;
 
+const requestToken = jwt.sign(
+  {
+    id: user._id.toString(),
+    username: user.username,
+  },
+  secret!
+);
+
+const gamesUrl = "/games";
+const deleteUrl = "/games/delete/";
+
+describe("Given a GET /games endpoint", () => {
   describe("When it receives a request", () => {
     test("Then it should respond with status 200 and the games requested", async () => {
       const expectedStatus = success.okCode;
 
       await request(app).get(gamesUrl).expect(expectedStatus);
+    });
+  });
+});
+
+describe("Given a Delete /games/delete/:gameId endpoint", () => {
+  describe("When it receives a request to delete a game", () => {
+    test("Then it should call the response method status with 200 and json with the game deleted", async () => {
+      const gameToDelete = mockWitcherGame;
+
+      const gamewithCreatedBy = { ...gameToDelete, createdBy: user._id };
+
+      const game = await Game.create(gamewithCreatedBy);
+
+      await request(app)
+        .delete(`${deleteUrl}${game._id}`)
+        .set("Authorization", `Bearer ${requestToken}`)
+        .set("Content-Type", "application/json")
+        .expect(success.okCode);
     });
   });
 });
